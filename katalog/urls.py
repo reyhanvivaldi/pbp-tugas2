1fc7622a72b431aa72d843ffd72f7f90c07e3f7f
Reyhan Vivaldi Adrian <reyhan.vivaldi@ui.ac.id>
2022-09-10 21:45:08 +0700

    modified procfile & mengerjakan lab 1 sesuai flow tutorial

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from katalog.views import show_katalog
+
 # TODO: Implement Routings Here
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_katalog, name='show_katalog')
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+# ambil dari models, karena perlu akses ke database
+data_katalog = CatalogItem.objects.all()
+
+# untuk dipanggil di template (html)
+context = {
+    'katalog_item': data_katalog,
+    'nama': 'Reyhan Vivaldi Adrian',
+    'npm': '2106750811'
+}
+
+def show_katalog(req):
+    return render(req, "katalog.html", context)   # render itu ngapain? biar masuk ke urls?
