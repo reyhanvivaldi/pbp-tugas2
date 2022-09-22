509e8fcfa5498258ba55379193014c4c942d2cb9
Reyhan Vivaldi Adrian <reyhan.vivaldi@ui.ac.id>
2022-09-22 09:54:47 +0700

    modify html function

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,13 +6,21 @@ from django.core import serializers
 # Create your views here.
 data_mywatchlist = MyWatchList.objects.all()
 
-context = {
+def show_mywatchlist_html(request):
+    t, f = 0, 0
+    for movie in data_mywatchlist:
+        if movie.watched == True:
+            t += 1
+        else:
+            t += 1
+
+    context = {
     'mywatchlist_item': data_mywatchlist,
     'nama': 'Reyhan Vivaldi Adrian',
-    'npm': '2106750811'
+    'npm': '2106750811',
+    'msg': "Selamat! Kamu sudah banyak menonton!" if t>=f else "Wah, kamu masih sedikit menonton!"
 }
 
-def show_mywatchlist_html(request):
     return render(request, "mywatchlist.html", context)
 
 def show_mywatchlist_xml(request):
