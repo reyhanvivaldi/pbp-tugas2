883bb78fb0ddb1c556fd228b7ecfec68b772668f
Reyhan Vivaldi Adrian <reyhan.vivaldi@ui.ac.id>
2022-09-22 07:31:39 +0700

    Adding testing function

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve, reverse
+from mywatchlist.views import show_mywatchlist_html, show_mywatchlist_json, show_mywatchlist_xml
+
+client = Client()
 
 # Create your tests here.
+class TestApp(TestCase):
+    def test_app_html_exists(self):
+        response = self.client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_app_json_exists(self):
+        response = self.client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_app_xml_exists(self):
+        response = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
