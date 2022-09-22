14f5728a819a04353443cadea1b8a23af884939b
Reyhan Vivaldi Adrian <reyhan.vivaldi@ui.ac.id>
2022-09-22 09:51:58 +0700

    modify models

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class MyWatchList(models.Model):
-    watched = models.CharField(max_length=50)
+    watched = models.BooleanField()
     title = models.CharField(max_length=255)
     rating = models.IntegerField()
     release_date = models.CharField(max_length=255)
